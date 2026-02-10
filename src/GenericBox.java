61610ee053a2b2250766c23a18ca8aaedc1c4468
Sweedal <sweedallewis@gmail.com>
2026-02-10 19:33:55 +0530

    Added new files

---
+++ src/GenericBox.java
@@ -0,0 +1,18 @@
+
+
+public class GenericBox<T> {
+
+    private T value;
+
+    public GenericBox(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
